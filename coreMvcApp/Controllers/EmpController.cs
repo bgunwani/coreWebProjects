ec24d3bb516599f72e00f53dd50ef75f2bf9a1c8
bgunwani <bhawna.gunwani@gmail.com>
2025-01-29 10:53:21 +0530

    Push Two

---
+++ coreMvcApp/Controllers/EmpController.cs
@@ -0,0 +1,93 @@
+﻿using coreMvcApp.Models;
+using coreMvcApp.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreMvcApp.Controllers
+{
+    public class EmpController : Controller
+    {
+        // GET: EmpController
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Employee List";
+            var employees = EmployeeRepository.GetAll();
+            return View(employees);
+        }
+
+        // GET: EmpController/Details/5
+        public ActionResult Details(int id)
+        {
+            var employee = EmployeeRepository.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: EmpController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmpController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                EmployeeRepository.Add(employee);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmpController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmpController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmpController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmpController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
