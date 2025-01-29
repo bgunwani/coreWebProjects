4bd6ccb4b940e8e445b0c4e5c67b7473a09ad117
bgunwani <bhawna.gunwani@gmail.com>
2025-01-29 11:31:19 +0530

    Push 4

---
+++ coreMvcApp/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace coreMvcApp.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        // Mock Credentials
+        private readonly string _username = "admin";
+        private readonly string _password = "password123";
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if(username == _username && password == _password)
+            {
+                HttpContext.Session.SetString("uname", username);
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Error = "Invalid Credentials..!";
+            return View();
+        }
+    }
+}
--- coreMvcApp/Program.cs
+++ coreMvcApp/Program.cs
@@ -3,6 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add Session Service to the container
+builder.Services.AddSession(option =>
+{
+    option.IdleTimeout = TimeSpan.FromMinutes(20);
+    option.Cookie.HttpOnly = true;
+    option.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,10 +22,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
