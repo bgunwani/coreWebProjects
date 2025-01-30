78bfb3ca69f12ecb96b578b1d314f1857ec48e7f
bgunwani <bhawna.gunwani@gmail.com>
2025-01-30 11:36:06 +0530

    Push 7

--- coreEFDependencyMiddlewareApp/Models/Employee.cs
+++ coreEFDependencyMiddlewareApp/Models/Employee.cs
@@ -6,5 +6,6 @@
         public string? Name { get; set; }
         public string? Designation { get; set; }
         public decimal Salary { get; set; }
+        public string? Email { get; set; }
     }
 }
