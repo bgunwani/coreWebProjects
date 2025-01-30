b5ea9bae36ac6f46246ee3373a6492eac347a67b
bgunwani <bhawna.gunwani@gmail.com>
2025-01-30 10:40:58 +0530

    Push

---
+++ coreEFDependencyMiddlewareApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace coreEFDependencyMiddlewareApp.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-Q8KMI7N;Database=SampleDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+    }
+}
---
+++ coreEFDependencyMiddlewareApp/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace coreEFDependencyMiddlewareApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Designation { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
